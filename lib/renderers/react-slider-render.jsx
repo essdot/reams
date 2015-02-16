f1f9ffe4e16f70a813ec9a2dfc7cdec8616f33d8
Justin Sippel essdot <justinsippel+gh@gmail.com>
2015-02-16 14:33:31 -0800

    Move slider render

---
+++ lib/renderers/react-slider-render.jsx
@@ -0,0 +1,11 @@
+var React = require('react')
+
+module.exports = render_slider
+
+function render_slider() {
+	var val =  this.state.value
+	var min =  this.props.min
+	var max =  this.props.max
+
+	return <input type="range" defaultValue={val} min={min} max={max} />
+}
