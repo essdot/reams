8a8488b70aba910f3f927d3ea0e52d43d6d0afb8
Justin Sippel essdot <justinsippel+gh@gmail.com>
2015-03-07 21:17:37 -0800

    Add draggable & drag-mirror

--- index.js
+++ index.js
@@ -1,22 +1,14 @@
 var autocomplete = require('./lib/widgets/autocomplete')
 var queryStream = require('./lib/modules/query-stream')
 var react_slider = require('./lib/widgets/react-slider')
-
+var drag_mirror = require('./lib/widgets/drag-mirror')
+var make_draggable = require('./lib/modules/draggable')
 
 var autocomplete_el = document.getElementById('autocomplete')
 var autocomplete_el_2 = document.getElementById('autocomplete2')
 var slider_el = document.getElementById('slider')
+var drag_mirror_el = document.getElementById('drag-mirror-wrapper')
 var auto = autocomplete(autocomplete_el, queryStream())
 var auto2 = autocomplete(autocomplete_el_2, queryStream())
 var slider = react_slider(slider_el)
-
-window._auto = auto
-window._auto2 = auto2
-window._slider = slider
-// window._auto.on('data', function(data) { console.log(data) })
-window._slider.on('data', function(data) { 
-	document.querySelector('.slider-display').innerHTML = '' + data
-})
-
-var React = require('react')
-var factory = React.createFactory()
+var mirror = drag_mirror(drag_mirror_el, 'img.jpg')
