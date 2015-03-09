29ae59fc6b20266cb09f6412b8aab5e45ee8cc33
Justin Sippel essdot <justinsippel+gh@gmail.com>
2015-03-08 21:12:52 -0700

    Fix test

--- test/react-slider.test.js
+++ test/react-slider.test.js
@@ -6,7 +6,7 @@ var react_slider = require('../lib/widgets/react-slider')
 test('slider', function(t) {
 	var el = dom()
 	var slider = react_slider(el)
-	
+
 	t.ok(slider)
 	t.equal(el, slider.component.parent_el)
 	t.end()
@@ -17,12 +17,12 @@ test('write sets value', function(t) {
 	var slider = react_slider(el, 0)
 
 	t.equal(slider.component.state.value, 0)
-	t.equal(slider.component.refs.input.value, '0')
+	t.equal(el.children[0].value, '0')
 
 	slider.write(1.5)
 
 	t.equal(slider.component.state.value, 1.5)
-	t.equal(slider.component.refs.input.value, '1.5')
+	t.equal(el.children[0].value, '1.5')
 
 	t.end()
 })
@@ -32,7 +32,7 @@ test('write queues value', function(t) {
 	var slider = react_slider(el, 0)
 
 	t.plan(1)
-	
+
 	slider.on('data', function(data) {
 		t.equal(data, 1.5)
 		t.end()
