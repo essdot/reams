ba36af4ad7013f3aaa75e054334fbcc702f283f1
Justin Sippel essdot <justinsippel+gh@gmail.com>
2015-03-08 18:38:31 -0700

    Reorganize assets

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ var autocomplete = require('./lib/widgets/autocomplete')
 var queryStream = require('./lib/modules/query-stream')
 var react_slider = require('./lib/widgets/react-slider')
 var make_swiper = require('./lib/widgets/swipe-compare')
-// var make_draggable = require('./lib/modules/draggable')
 var make_onion_skin = require('./lib/widgets/onion-skin-compare')
 
 var autocomplete_el = document.getElementById('autocomplete')
@@ -10,8 +9,12 @@ var autocomplete_el_2 = document.getElementById('autocomplete2')
 var slider_el = document.getElementById('slider')
 var swiper_el = document.getElementById('swiper-wrapper')
 var onion_skin_el = document.getElementById('onion-skin-wrapper')
+
+var img1_url = 'img/img1.png'
+var img2_url = 'img/img2.png'
+
 var auto = autocomplete(autocomplete_el, queryStream())
 var auto2 = autocomplete(autocomplete_el_2, queryStream())
 var slider = react_slider(slider_el)
-var swiper = make_swiper(swiper_el, 'img1.png', 'img2.png')
-var onion_skin = make_onion_skin(onion_skin_el, 'img2.png', 'img1.png')
+var swiper = make_swiper(swiper_el, img1_url, img2_url)
+var onion_skin = make_onion_skin(onion_skin_el, img2_url, img1_url)
