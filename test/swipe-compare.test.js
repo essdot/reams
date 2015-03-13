81ed97d74e91e4bb72f4f07e3e101508e052d002
Justin Sippel <justin.sippel@urbanairship.com>
2015-03-12 20:42:36 -0700

    Streamify swipe-compare

--- test/swipe-compare.test.js
+++ test/swipe-compare.test.js
@@ -6,8 +6,8 @@ var swipe_compare = require('../lib/widgets/swipe-compare')
 function create_swiper() {
 	var el = dom('<div class="swiper-wrapper"></div>')
 	var swiper = swipe_compare(el, 'img1.png', 'img2.png')
-	swiper.get_image_dimensions = function() { return {height: 400, width: 750}}
-	swiper.ready()
+	swiper.component.get_image_dimensions = function() { return {height: 400, width: 750}}
+	swiper.component.images_loaded()
 
 	return swiper
 }
@@ -15,20 +15,31 @@ function create_swiper() {
 test('swipe compare', function(t) {
 	var swiper = create_swiper()
 
-	t.equal(swiper.state.percent, .5)
-	t.deepEqual(swiper.props.dimensions, {height: 400, width: 750})
+	t.equal(swiper.component.state.percent, .5)
+	t.deepEqual(swiper.component.props.dimensions, {height: 400, width: 750})
+	t.end()
+})
+
+test('write sets percent', function(t) {
+	var swiper = create_swiper()
+
+	t.equal(swiper.component.state.percent, .5)
+
+	swiper.write(.7)
+	t.equal(swiper.component.state.percent, .7)
+
 	t.end()
 })
 
 test('calculates percentages correctly', function(t) {
 	var swiper = create_swiper()
 
-	swiper.set_percent(.3)
+	swiper.write(.3)
 
-	t.equal(swiper.state.percent, .3)
-	t.equal(swiper.state.big_percent, 30)
-	t.equal(swiper.state.reverse_big_percent, 70)
-	t.equal(swiper.state.percent_string, '30%')
-	t.equal(swiper.state.reverse_percent_string, '70%')
+	t.equal(swiper.component.state.percent, .3)
+	t.equal(swiper.component.state.big_percent, 30)
+	t.equal(swiper.component.state.reverse_big_percent, 70)
+	t.equal(swiper.component.state.percent_string, '30%')
+	t.equal(swiper.component.state.reverse_percent_string, '70%')
 	t.end()
 })
