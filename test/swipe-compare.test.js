500682bc65ffc8a18f449af58c00a5a4e605ae24
Justin Sippel essdot <justinsippel+gh@gmail.com>
2015-03-14 18:32:01 -0700

    Add mousedown test

--- test/swipe-compare.test.js
+++ test/swipe-compare.test.js
@@ -1,6 +1,8 @@
 var test = require('tape')
 var dom = require('dom-sandbox')
 
+var mousedown = require('../lib/dom-utils/mousedown')
+
 var swipe_compare = require('../lib/widgets/swipe-compare')
 
 function create_swiper() {
@@ -45,4 +47,23 @@ test('calculates percentages correctly', function(t) {
 	t.equal(swiper.component.state.percent_string, '30%')
 	t.equal(swiper.component.state.reverse_percent_string, '70%')
 	t.end()
+})
+
+test('mousedown changes percent', function(t){
+	var swiper = create_swiper()
+	var wrapper_width = swiper.component.props.dimensions.width
+	var orig_console_warn = console.warn
+	console.warn = function() {} // suppress React warnings
+
+	t.plan(2)
+
+	t.equal(swiper.component.state.percent, .5)
+
+	swiper.on('data', function(data) {
+		t.equal(data, (1 / 3))
+		t.end()
+	})
+
+	mousedown(swiper.component.getDOMNode(), wrapper_width / 3, 15)
+	console.warn = orig_console_warn
 })
